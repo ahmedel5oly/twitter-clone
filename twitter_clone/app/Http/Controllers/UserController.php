d95c0a85672b3e59dc2766bc8888c74470580ab5
Ahmed <aelkholy912@gmail.com>
2023-11-17 01:18:34 +0200

    submit html form into db

--- twitter_clone/app/Http/Controllers/UserController.php
+++ twitter_clone/app/Http/Controllers/UserController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     //why function should use public/private
-    public function register(){
+    public function register(Request $request){
+        $incomingFields= $request->validate([
+            'username'=>'required',
+            'email'=>'required',
+            'password'=>'required'
+        ]);
+        User::create($incomingFields);
         return "they don't know me son";
     }
 }
