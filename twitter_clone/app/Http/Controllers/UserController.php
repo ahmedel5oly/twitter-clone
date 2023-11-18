985a0efdb77b78e0b46b41352d8ed472dc7ea876
Ahmed <aelkholy912@gmail.com>
2023-11-18 02:17:37 +0200

    add validation

--- twitter_clone/app/Http/Controllers/UserController.php
+++ twitter_clone/app/Http/Controllers/UserController.php
@@ -4,16 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     //why function should use public/private
     public function register(Request $request){
         $incomingFields= $request->validate([
-            'username'=>'required',
-            'email'=>'required',
-            'password'=>'required'
+            'username'=>['required', 'min:3','max:20',Rule::unique('users','username')],
+            'email'=>['required','email',Rule::unique('users','email')],
+            'password'=>['required','min:8','confirmed'] //confirmed make sure the 2 password field are the same
         ]);
+        //hashing password in db
+        $incomingFields['password']=bcrypt($incomingFields['password']);
         User::create($incomingFields);
         return "they don't know me son";
     }
