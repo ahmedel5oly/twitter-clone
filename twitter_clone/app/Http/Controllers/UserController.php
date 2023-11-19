4c5ac8f695527ea736e237ef619439c2bbfe7e36
Ahmed <aelkholy912@gmail.com>
2023-11-19 11:14:15 +0200

    add login auth

--- twitter_clone/app/Http/Controllers/UserController.php
+++ twitter_clone/app/Http/Controllers/UserController.php
@@ -5,9 +5,33 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use PhpParser\Builder\Function_;
+use PhpParser\Node\Expr\FuncCall;
 
 class UserController extends Controller
 {
+    public function showCorrectHomepage(){
+        if(auth()->check()){
+            return view('homepage-feed');
+        } else{
+            return view('homepage');
+        }
+    }
+    
+    public Function login(Request $request){
+        $incomingFields=$request->validate([
+            'loginusername'=>['required'],
+            'loginpassword'=>['required']
+        ]);
+        if(auth()->attempt(['username'=>$incomingFields['loginusername'], 'password'=>$incomingFields['loginpassword']])){
+            //use session method to send cookie to browser to keep the user loged in all over the pages
+            $request->session()->regenerate();
+            return 'done mf';
+        }else{
+            return 'no mf';
+        }
+    }
+
     //why function should use public/private
     public function register(Request $request){
         $incomingFields= $request->validate([
--- twitter_clone/routes/web.php
+++ twitter_clone/routes/web.php
@@ -15,5 +15,6 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', [HomeController::class, 'homepage']);
+Route::get('/', [UserController::class, 'showCorrectHomepage']);
 Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
